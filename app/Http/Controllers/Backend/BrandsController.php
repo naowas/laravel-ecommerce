4a8adfca955158c2773a5e210a207a44e6090af9
Naowas Morshed Eimon <naowas.morshed.eimon@gmail.com>
2020-07-28 23:34:10 +0600

    bug fix, server deploy ready

--- app/Http/Controllers/Backend/BrandsController.php
+++ app/Http/Controllers/Backend/BrandsController.php
@@ -1,141 +1,125 @@
 <?php
-
 namespace App\Http\Controllers\Backend;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\Brand;
-use Intervention\Image\ImageManagerStatic as Image;
-use Illuminate\Support\Facades\File;
 
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class BrandsController extends Controller
 {
-     public function __construct(Type $var = null)
+    public function __construct()
     {
-        $this ->middleware('auth:admin');
+        $this->middleware('auth:admin');
     }
-   public function index()
-   {
-     $brands = Brand::orderBy('id', 'desc') -> get();
-     return view('backend.pages.brands.index', compact('brands') );
-   }
+    public function index()
+    {
+        $brands = Brand::orderBy('id', 'desc')->get();
+        return view('backend.pages.brands.index', compact('brands'));
+    }
 
-   public function create(){
+    public function create()
+    {
         return view('backend.pages.brands.create');
 
-   }
+    }
 
-   public function store(Request $request){
+    public function store(Request $request)
+    {
 
-     $this->validate($request,[
+        $this->validate($request, [
 
-      'name' => 'required',
-      'image' => 'nullable|image',
+            'name' => 'required',
+            'image' => 'nullable|image',
 
-    ],
-    [
-      'image.image' => 'only JPG, JPEG, PNG, GIF is allowed',
-      'name.required' => 'Provide a categoy name',
-    ]);
+        ],
+            [
+                'image.image' => 'only JPG, JPEG, PNG, GIF is allowed',
+                'name.required' => 'Provide a categoy name',
+            ]);
 
-      $brand = new Brand();
-      $brand -> name = $request ->name;
-      $brand -> description = $request ->description;
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->description = $request->description;
 
-        if(($request->image)>0){
-
+        if (($request->image) > 0) {
 
             $image = $request->file('image');
-            $img = time(). '.'. $image->getClientOriginalExtension();
-            $location = public_path('images/brands/' .$img);
+            $img = time() . '.' . $image->getClientOriginalExtension();
+            $location =('images/brands/' . $img);
             Image::make($image)->save($location);
-            $brand -> image = $img;
+            $brand->image = $img;
 
         }
-      $brand ->save();
+        $brand->save();
 
-      session()->flush('success', 'New Brand has been added');
-      return redirect()->route('admin.brands');
+        session()->flush('success', 'New Brand has been added');
+        return redirect()->route('admin.brands');
 
+    }
 
-   }
-
-    public function edit($id){
-
+    public function edit($id)
+    {
 
         $brand = Brand::find($id);
 
-        if(!is_null($brand)){
-        return view('backend.pages.brands.edit' , compact('brand'));
+        if (!is_null($brand)) {
+            return view('backend.pages.brands.edit', compact('brand'));
 
+        } else {
+            return redirect()->route('admin.brands');
         }
-        else{
-          return redirect()->route('admin.brands');
-        }
-
-
-
-
 
     }
 
+    public function update(Request $request, $id)
+    {
 
-public function update(Request $request, $id){
+        $this->validate($request, [
 
-     $this->validate($request,[
+            'name' => 'required',
+            'image' => 'nullable|image',
 
-      'name' => 'required',
-      'image' => 'nullable|image',
+        ],
+            [
+                'image.image' => 'only JPG, JPEG, PNG, GIF is allowed',
+                'name.required' => 'Provide a categoy name',
+            ]);
 
-    ],
-    [
-      'image.image' => 'only JPG, JPEG, PNG, GIF is allowed',
-      'name.required' => 'Provide a categoy name',
-    ]);
+        $brand = Brand::find($id);
+        $brand->name = $request->name;
+        $brand->description = $request->description;
+        if (($request->image) > 0) {
 
-      $brand = Brand::find($id);
-      $brand -> name = $request ->name;
-      $brand -> description = $request ->description;
-        if(($request->image)>0){
-
-          if(File::exists('images/brands/'. $brand -> image))
-          {
-            File::delete('images/brands/'. $brand -> image);
-          }
-
-
+            if (File::exists('images/brands/' . $brand->image)) {
+                File::delete('images/brands/' . $brand->image);
+            }
 
             $image = $request->file('image');
-            $img = time(). '.'. $image->getClientOriginalExtension();
-            $location = public_path('images/brands/' .$img);
+            $img = time() . '.' . $image->getClientOriginalExtension();
+            $location = ('images/brands/' . $img);
             Image::make($image)->save($location);
-            $brand -> image = $img;
-
-
+            $brand->image = $img;
 
         }
-      $brand ->save();
+        $brand->save();
 
-      session()->flush('success', 'New Brand has been Updated');
-      return redirect()->route('admin.brands');
+        session()->flash('success', 'New Brand has been Updated');
+        return redirect()->route('admin.brands');
 
-   }
-
+    }
 
     public function delete($id)
     {
         $brand = Brand::find($id);
-        if(!is_null($brand)){
+        if (!is_null($brand)) {
 
+            if (File::exists('images/brands/' . $brand->image)) {
+                File::delete('images/brands/' . $brand->image);
+            }
 
-          if(File::exists('images/brands/'. $brand -> image))
-          {
-            File::delete('images/brands/'. $brand -> image);
-          }
-
-            $brand -> delete();
-
-
+            $brand->delete();
 
         }
 
--- app/Http/Controllers/Backend/ProductsController.php
+++ app/Http/Controllers/Backend/ProductsController.php
@@ -1,44 +1,43 @@
 <?php
 
 namespace App\Http\Controllers\Backend;
-use App\Http\Controllers\Controller;
 
-use App\Models\ProductImage;
+use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\ProductImage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Intervention\Image\ImageManagerStatic as Image;
 
-
-
 class ProductsController extends Controller
 {
-     public function __construct(Type $var = null)
+    public function __construct(Type $var = null)
     {
-        $this ->middleware('auth:admin');
+        $this->middleware('auth:admin');
     }
-    public function index(){
+    public function index()
+    {
         $products = Product::orderBy('id', 'desc')->get();
 
-        return view('backend.pages.product.index')-> with('products', $products);
+        return view('backend.pages.product.index')->with('products', $products);
     }
 
-     public function create(){
+    public function create()
+    {
         return view('backend.pages.product.create');
     }
 
-
-
-    public function edit($id){
+    public function edit($id)
+    {
         $product = Product::find($id);
 
-        return view('backend.pages.product.edit')-> with('product', $product);
+        return view('backend.pages.product.edit')->with('product', $product);
     }
 
     public function store(Request $request)
-  {
+    {
 
-    $request->validate([
+        $request->validate([
             'title' => 'required|max:255',
             'description' => 'required',
             'price' => 'required|numeric',
@@ -46,45 +45,41 @@ class ProductsController extends Controller
             'category_id' => 'required',
             'brand_id' => 'required',
 
-    ]);
+        ]);
 
+        $product = new Product;
 
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
 
-    $product = new Product;
+        $product->slug = Str::slug($request->title);
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+        $product->admin_id = 1;
+        $product->save();
 
-    $product->title = $request->title;
-    $product->description = $request->description;
-    $product->price = $request->price;
-    $product->quantity = $request->quantity;
+        if (count($request->product_image) > 0) {
+            $i = 0;
+            foreach ($request->product_image as $image) {
+                $img = time() . $i . '.' . $image->getClientOriginalExtension();
+                $location = 'images/products/' . $img;
+                Image::make($image)->save($location);
 
-    $product->slug = Str::slug($request->title);
-    $product->category_id = $request->category_id;
-    $product->brand_id = $request->brand_id;
-    $product->admin_id = 1;
-    $product->save();
+                $product_image = new ProductImage;
+                $product_image->product_id = $product->id;
+                $product_image->image = $img;
+                $product_image->save();
+                $i++;
+            }
+        }
 
-    if (count($request->product_image) > 0) {
-      $i = 0;
-      foreach ($request->product_image as $image) {
-        $img = time() . $i .'.'. $image->getClientOriginalExtension();
-        $location = 'images/products/' .$img;
-        Image::make($image)->save($location);
-
-        $product_image = new ProductImage;
-        $product_image->product_id = $product->id;
-        $product_image->image = $img;
-        $product_image->save();
-        $i++;
-      }
+        return redirect()->route('admin.products');
     }
 
-    return redirect()->route('admin.products');
-  }
-
-
-
-    public function update(Request $request, $id){
-
+    public function update(Request $request, $id)
+    {
 
         $validatedData = $request->validate([
             'title' => 'required|max:255',
@@ -94,57 +89,55 @@ class ProductsController extends Controller
             'category_id' => 'required',
             'brand_id' => 'required',
 
-
-
-
-
         ]);
 
         $product = Product::find($id);
-         $product -> title = $request -> title;
-         $product -> description = $request -> description;
-         $product -> price = $request -> price;
-         $product -> quantity = $request -> quantity;
-          $product->category_id = $request->category_id;
-    $product->brand_id = $request->brand_id;
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+        $product->save();
 
-         $product-> save();
+      if (count($request->product_image) > 0) {
+            $i = 0;
+            foreach ($request->product_image as $image) {
+                $img = time() . $i . '.' . $image->getClientOriginalExtension();
+                $location = 'images/products/' . $img;
+                Image::make($image)->resize(320, 240)->save($location);
 
-        // if(count($request->product_image)>0){
-        //     foreach($request->product_image as $image){
-
-        //     $img = time(). '.'. $image->getClientOriginalExtension();
-        //     $location = public_path('images/products/' .$img);
-        //     Image::make($image)->save($location);
-
-        //     $product_image= new ProductImage;
-        //     $product_image -> product_id = $product -> id;
-        //     $product_image -> image = $img ;
-        //     $product_image -> save();
-
-        //     }
-        // }
-         session()->flash('success', 'Product has been updated !');
-
-         return redirect()->route('admin.products');
-
-
-
-
-
-    }
-
-    public function delete($id)
-    {
-        $product = Product::find($id);
-        if(!is_null($product)){
-
-            $product -> delete();
-
+                $product_image = new ProductImage;
+                $product_image->product_id = $product->id;
+                $product_image->image = $img;
+                $product_image->save();
+                $i++;
+            }
         }
+        session()->flash('success', 'Product has been updated !');
 
-        session()->flash('success', 'Product has been deleted !');
+        return redirect()->route('admin.products');
 
-        return back();
     }
+
+     public function delete($id)
+  {
+    $product = Product::find($id);
+    if (!is_null($product)) {
+      $product->delete();
+    }
+    // Delete all images
+    foreach ($product->images as $img) {
+      // Delete from path
+      $file_name = $img->image;
+      if (file_exists("images/products/".$file_name)) {
+        unlink("images/products/".$file_name);
+      }
+
+      $img->delete();
+    }
+    session()->flash('success', 'Product has deleted successfully !!');
+    return back();
+
+  }
 }
